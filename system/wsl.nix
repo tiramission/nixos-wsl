85954533a75f82025c88945fdb2e9c07b0b04070
tiramission <37465086+tiramission@users.noreply.github.com>
2026-04-30 06:43:34 +0000

    wsl: enable driver

--- system/wsl.nix
+++ system/wsl.nix
@@ -9,6 +9,8 @@
   wsl.enable = true;
   wsl.defaultUser = "nixos";
   wsl.ssh-agent.enable = true;
+  wsl.useWindowsDriver = true;
+  wsl.wslConf.interop.appendWindowsPath = false;
 
   system.stateVersion = config.system.nixos.release;
 }
