2e0192215df9f4b013aa97316bca678e2399d675
tiramission <37465086+tiramission@users.noreply.github.com>
2026-05-18 17:04:03 +0800

    go: add multi go

--- home/programs.d/go.nix
+++ home/programs.d/go.nix
@@ -1,8 +1,19 @@
 {pkgs, ...}: {
   programs.go = {
     enable = true;
-    env.CGO_ENABLED = "0";
+    env = {
+      CGO_ENABLED = "0";
+      GOTOOLCHAIN = "local";
+    };
   };
 
-  home.packages = [pkgs.gopls];
+  home.packages = [
+    pkgs.gopls
+
+    (pkgs.runCommandLocal "multi-go" {} ''
+      mkdir -p $out/bin
+      ln -s ${pkgs.go_1_26}/bin/go $out/bin/go1.26
+      ln -s ${pkgs.go_1_25}/bin/go $out/bin/go1.25
+    '')
+  ];
 }
